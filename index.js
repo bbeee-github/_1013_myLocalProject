53cce4f0a2cba19c2002c57a0ccbd9e8b48c1504
danbee511 <shmkyq@gmail.com>
2023-10-13 10:02:00 +0900

    구구단 함수 완성

---
+++ index.js
@@ -0,0 +1,8 @@
+// 구구단
+const makeMultiplecation = () => {
+    for (let i = 1; i <= 9; i++) {
+        for (let j = 2; j <= 9; j++) {
+            console.log(`${'j'} * ${'i'} = ${'j * i'}`)
+        }
+    }
+};
